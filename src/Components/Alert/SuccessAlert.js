678fee06e1b480619234769fef443004c6967912
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2024-04-25 17:43:05 +0530

    Update SuccessAlert.js

--- src/Components/Alert/SuccessAlert.js
+++ src/Components/Alert/SuccessAlert.js
@@ -1,14 +1,20 @@
-import React, { useContext } from 'react';
-import Alert from '@mui/material/Alert';
-import CheckIcon from '@mui/icons-material/Check';
-import "./Alert.css"
+import  React, { useContext } from 'react';
 import { IMDBContext } from '../../Context';
-export default function SuccessAlert() {
-  const{successCreateAlert,successDeleteAlert,successUpdateAlert}=useContext(IMDBContext)
+
+export default function FailureAlert() {
+  const{failureCreateAlert,failureUpdateAlert,failureDeleteAlert,failureAddAlertWish,
+    failureDeleteAlertWish,failureAddAlertWishAlready}=useContext(IMDBContext)
   return (
-    <div className='success-alert'>
-      {successCreateAlert?"Successfully Created":successUpdateAlert?
-    "Successfully Updated":successDeleteAlert?"Successfully Deleted":""}
+    <div className='failure-alert'>
+    {failureCreateAlert?"Can not Add Data"
+    :failureUpdateAlert?"Can not Update Data"
+    :failureDeleteAlert?"Can not Delete Data"
+    :failureAddAlertWish?"Can not Add Wish List"
+    :failureDeleteAlertWish?"Can not Remove Movie From Wish List"
+    :failureAddAlertWishAlready?"Movie Already In Wish List"
+    :""
+    }
     </div>
   );
 }
+
