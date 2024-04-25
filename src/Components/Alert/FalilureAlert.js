7676df521e2b506bec3f9db152993c775496dd6e
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2024-04-25 17:44:26 +0530

    Update FalilureAlert.js

--- src/Components/Alert/FalilureAlert.js
+++ src/Components/Alert/FalilureAlert.js
@@ -3,19 +3,17 @@ import { IMDBContext } from '../../Context';
 
 export default function FailureAlert() {
   const{failureCreateAlert,failureUpdateAlert,failureDeleteAlert,failureAddAlertWish,
-    failureDeleteAlertWish,failureAddAlertWishAlready}=useContext(IMDBContext);
-
-    let content=failureCreateAlert?"Can not Create Data"
+    failureDeleteAlertWish,failureAddAlertWishAlready}=useContext(IMDBContext)
+  return (
+    <div className='failure-alert'>
+    {failureCreateAlert?"Can not Add Data"
     :failureUpdateAlert?"Can not Update Data"
     :failureDeleteAlert?"Can not Delete Data"
     :failureAddAlertWish?"Can not Add Wish List"
     :failureDeleteAlertWish?"Can not Remove Movie From Wish List"
     :failureAddAlertWishAlready?"Movie Already In Wish List"
     :""
-    
-  return (
-    <div className='failure-alert'>
-    {content}
+    }
     </div>
   );
 }
